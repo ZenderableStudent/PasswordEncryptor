dc9a079d9eedb0ea57d9976fe8c5b6917296cc35
Karol Stumski <stumski.karol@gmail.com>
2021-05-16 17:23:32 +0200

    Remove unecessary method

--- PasswordEncryptor/AssymetricEncryption.cs
+++ PasswordEncryptor/AssymetricEncryption.cs
@@ -56,19 +56,5 @@ namespace PasswordEncryptor
                 }
             }
         }
-
-        public static string GenerateTestString()
-        {
-            Guid opportinityId = Guid.NewGuid();
-            Guid systemUserId = Guid.NewGuid();
-            string currentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-
-            StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("opportunityid={0}", opportinityId.ToString());
-            sb.AppendFormat("&systemuserid={0}", systemUserId.ToString());
-            sb.AppendFormat("&currenttime={0}", currentTime);
-
-            return sb.ToString();
-        }
     }
 }
